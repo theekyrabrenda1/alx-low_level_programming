fd93e90c72af92328f62b42e0bed1f2b747f9848
theekyrabrenda1 <theekyrabrenda@gmail.com>
2023-11-06 18:26:10 -0800

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: prints name of file
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+printf("The name of the file is: %s\n", __FILE__);
+return (0);
+}
