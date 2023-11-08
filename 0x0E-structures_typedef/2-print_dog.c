7fc2e303c936f0f3dbde9c7a4225f2f0e8c98f56
theekyrabrenda1 <theekyrabrenda@gmail.com>
2023-11-07 16:14:13 -0800

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,6 @@ return;
 }
 if (d->name == NULL)
 d->name = "(nil)";
-if (d->owner == NULL)
-d->owner = "(nil)";
-
 printf("Name: %s\n", d->name);
 printf("Age: %.1f\n", d->age);
 printf("Owner: %s\n", d->owner);
